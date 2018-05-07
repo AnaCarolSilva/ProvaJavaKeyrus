eba610058b81c676d9322caec4f130e260bb61b3
= <=>
2018-05-06 23:10:22 -0300

    testando

---
+++ MyStore-master/sql/FormatacaoData.sql
@@ -0,0 +1,8 @@
+---Continuaçao da questão 2
+
+DECLARE @P_DATE;
+DECLARE @time time(4) = '17:43:48.0';
+  
+DECLARE @datetime datetime = '23/11/2015';  
+
+SELECT @P_DATE AS '@datetime', @time AS '@time';  
---
+++ MyStore-master/src/main/java/com/example/mystore/MystoreApplication.java
@@ -0,0 +1,23 @@
+package com.example.mystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystoreApplication.class, args);
+
+
+//JSON QUESTÃO 4
+//a)
+//{"id":1,"name":"apple"} 
+//{"id":2,"name":"orange"}
+
+
+
+//b)
+//{"id":1,"name":"apple"}
+	}
+}
